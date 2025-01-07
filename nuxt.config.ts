5811e6cf10a2671cf079315c9e124fb7475aa92f
Nicolas <nicolas.djgoncalves@gmail.com>
2025-01-07 06:54:38 +0100

    Icone et Titre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  css: ['~/assets/main.css'],
-
-  runtimeConfig: {
-      mongoUrl: process.env.MONGODB_URL
-  },
-  nitro: {
-      plugins: ['~/server/plugins/mongodb.ts']
-  },
-
-  devtools: { enabled: true },
-})
+    compatibilityDate: '2024-11-01',
+    css: ['~/assets/main.css'],
+    app: {
+        head: {
+            title: 'CatMash : Votez pour le plus beau chat'
+        }
+    },
+    runtimeConfig: {
+            mongoUrl: process.env.MONGODB_URL
+    },
+    nitro: {
+        plugins: ['~/server/plugins/mongodb.ts']
+    },
+    devtools: { enabled: true },
+});
