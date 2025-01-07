aa048b1a4ac55dea0c82aecb843c68718f15a5c4
Nicolas <nicolas.djgoncalves@gmail.com>
2025-01-07 06:55:54 +0100

    Suppression de debug

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -3,10 +3,6 @@ import mongoose from 'mongoose';
 
 export default async (_nitroApp: Nitro) => {
     const config = useRuntimeConfig();
-  
-    try {
-      await mongoose.connect(config.mongoUrl);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+
+    await mongoose.connect(config.mongoUrl);
+};
